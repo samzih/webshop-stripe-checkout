ccec43121caeca4c4058845e321a514993ab6722
samzih <samzih1337@gmail.com>
2023-09-09 13:51:46 +0200

    Bcrypt and hashing of password

--- server/src/controllers/customers.controller.js
+++ server/src/controllers/customers.controller.js
@@ -1,5 +1,6 @@
 const { initStripe } = require('../stripe');
 const stripe = initStripe();
+const bcrypt = require('bcrypt');
 
 const createCustomer = async (req, res) => {
 
@@ -14,9 +15,19 @@ const createCustomer = async (req, res) => {
 
     // If everything goes well and the customer is created on stripe
     if (customer) {
-        // Only if customer object comes back then we want to hash the password from req.body using (bcrypt?)
-        // We want to write in {} with fs (file system) to our users.json db (remeber to first use fs.readFile then fs.writeFile)
-        const newUser = {} // name, email, password (hashed), id (customer.id, looks like this: cus_ObLp3Od2JT15uJ)
+
+        const newUser = { name: customer.name, email: customer.email, password: 'password', id: customer.id } // name, email, password (hashed), id (customer.id, looks like this: cus_ObLp3Od2JT15uJ)
+        
+        // Only if customer object comes back then we want to hash the password from req.body using bcrypt
+        (async () => {
+            // (auto-gen a salt and hash):
+            const hashedPassword = await bcrypt.hash(password, 10);
+            console.log('Hashed password', hashedPassword);
+            // Store hash in your password DB. (first store into newUser object with dot notation like this: newUser.password = hashedPassword)
+        })();
+
+        // Write in {} with fs (file system) to our users.json db (remeber to first use fs.readFile then fs.writeFile)
+        
         
     }
 
