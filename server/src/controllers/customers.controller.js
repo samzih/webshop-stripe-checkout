c6ac06a3a184511ff641597ce4c08be13fb3632a
samzih <samzih1337@gmail.com>
2023-09-09 19:33:22 +0200

    New user stores in db with hashed password

--- server/src/controllers/customers.controller.js
+++ server/src/controllers/customers.controller.js
@@ -1,10 +1,10 @@
 const { initStripe } = require('../stripe');
 const stripe = initStripe();
 const bcrypt = require('bcrypt');
+const { readFile, writeFile } = require('fs/promises');
+const usersDB = './src/db/users.json';
 
 const createCustomer = async (req, res) => {
-
-    console.log(req.body);
 
     const { name, email, password } = req.body;
     
@@ -13,27 +13,26 @@ const createCustomer = async (req, res) => {
         email: email,
     });
 
-    // If everything goes well and the customer is created on stripe
+    // Only if customer object from Stripe comes back then create a new user obj and hash the password using bcrypt to then store the new user in DB
     if (customer) {
+        const newUser = { name: customer.name, email: customer.email, password: '', id: customer.id };
 
-        const newUser = { name: customer.name, email: customer.email, password: 'password', id: customer.id } // name, email, password (hashed), id (customer.id, looks like this: cus_ObLp3Od2JT15uJ)
-        
-        // Only if customer object comes back then we want to hash the password from req.body using bcrypt
-        (async () => {
-            // (auto-gen a salt and hash):
-            const hashedPassword = await bcrypt.hash(password, 10);
-            console.log('Hashed password', hashedPassword);
-            // Store hash in your password DB. (first store into newUser object with dot notation like this: newUser.password = hashedPassword)
-        })();
+        // Hash password
+        const hashedPassword = await bcrypt.hash(password, 10);
+        newUser.password = hashedPassword;
 
-        // Write in {} with fs (file system) to our users.json db (remeber to first use fs.readFile then fs.writeFile)
-        
-        
+        // Read & then write new user to DB
+        try {
+            const usersData = JSON.parse(await readFile(usersDB, 'utf-8'));
+            usersData.push(newUser);
+            await writeFile(usersDB, JSON.stringify(usersData, null, 2), 'utf-8');
+            res.status(201).json({ message: `Användaren (${name}) med email: ${email} har registrerats!` });
+        } catch (error) {
+            console.error('Error adding user:', error);
+            res.status(500).json({ error: 'Internal server error adding user' });
+        }
     }
 
-    // console.log(customer.id, customer.name, customer.email);
-    res.status(201).json(`Användaren (${name}) med email: ${email} har registrerats!`);
-    
 }
 
 module.exports = { createCustomer };
